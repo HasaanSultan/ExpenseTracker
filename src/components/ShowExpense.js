b43e8e484ac028c894db65f5872e49000f7eae18
HasaanSultan <Hasaansultan0@gmail.com>
2020-02-28 18:42:29 +0500

    Seacrch Functionality

--- src/components/ShowExpense.js
+++ src/components/ShowExpense.js
@@ -4,6 +4,7 @@ export const ShowExpense = ({expense}) => {
   const [data,setData]= useState([])
   const [sort,setSort]= useState(false)
   const [amountSort,setAmount]= useState(false)
+  const [filterList,Setfilter]= useState([])
 
   function SortByDate(){
     const newdata=[...data]
@@ -35,13 +36,40 @@ export const ShowExpense = ({expense}) => {
     setData(data=>[...data,expense]);
   },[expense])
 
+  const search=(e)=>{
+    if (e.target.value !== "") {
+     const newdata = data.filter(item => {
+       debugger
+        const lc = item.date.toLowerCase() + item.amount.toLowerCase() ;
+        const filter = e.target.value.toLowerCase();
+               return lc.includes(filter);
+      });
+      debugger
+     Setfilter(newdata) 
+    }
+    else
+    {
+      Setfilter([])
+    }
+  }
 
   return (
     <>
     
     <h3>expenses</h3>
+    <input type='text' name='search' placeholder="Search" onChange={(e)=>search(e)} />
     <button onClick={()=>SortByDate()}>Sort By Dates</button>
     <button onClick={()=>SortByAmount()}>Sort By Amount</button>
+    
+    
+    {filterList.length !==0 &&
+    <ul className="list">
+      {filterList.map(item=>{
+        return<li key={item.id} className='plus'>Date : {item.date}   Ammount : {item.amount} <button onClick={() => deleteTransaction(item.id)} className="delete-btn">x</button></li>      })
+        }
+      </ul>
+      }
+    <hr />
     <ul className="list">
       {data.map(item=>{
        return (<li key={item.id} className='plus'>Date : {item.date}   Ammount : {item.amount} <button onClick={() => deleteTransaction(item.id)} className="delete-btn">x</button></li>)
